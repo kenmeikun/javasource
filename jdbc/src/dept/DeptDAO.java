7e8fcba7701bf890a89785414bfd43b8f58fd150
park <parkgod97@naver.com>
2021-09-30 15:58:28 +0900

    database 커넥션

--- jdbc/src/dept/DeptDAO.java
+++ jdbc/src/dept/DeptDAO.java
@@ -1,5 +1,30 @@
 package dept;
 
-public class DeptDAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
-}
+public class DeptDAO {
+	//empDAO static ,Connection
+	static {
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+		} catch (ClassNotFoundException e) {
+		
+			e.printStackTrace();
+		}
+	}//static
+	
+	public static Connection getConnection() {
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String user = "c##scott";
+		String password = "tiger";
+		Connection con= null;
+		
+		try {
+			con=DriverManager.getConnection(url,user,password);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+}//
