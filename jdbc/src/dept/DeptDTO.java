fb25a5400ec99b27dcd0cfb991b2c57fea7f536b
park <parkgod97@naver.com>
2021-09-30 14:40:23 +0900

    dept

--- jdbc/src/dept/DeptDTO.java
+++ jdbc/src/dept/DeptDTO.java
@@ -1,5 +1,23 @@
 package dept;
 
-public class DeptDTO {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+//테이블 모양과 똑같이 작성
+
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeptDTO {
+	private int deptNo;
+	private String dname;
+	private String loc;
+	
+	//생성자,getter,setter,toString
+	
 }
