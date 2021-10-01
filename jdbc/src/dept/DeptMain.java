20bd1dd6c3478438ecc2a41afcbd7d4a18489ad6
park <parkgod97@naver.com>
2021-10-01 11:18:57 +0900

    메인 메뉴 작성

--- jdbc/src/dept/DeptMain.java
+++ jdbc/src/dept/DeptMain.java
@@ -1,9 +1,17 @@
 package dept;
 
+import java.util.Scanner;
+
 public class DeptMain {
 
 	public static void main(String[] args) {
 		//dsd
+		boolean run =true;
+		Scanner sc= new Scanner(System.in);
+		
+		while (run) {
+			
+		}
 	}//
 
 }
